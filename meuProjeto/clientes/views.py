7a8488447c018bae1973f1f3d3969066b74e604a
VictorSad26 <victor.sad@hotmail.com>
2024-05-08 12:11:53 -0300

    realizando o crud de clientes

--- meuProjeto/clientes/views.py
+++ meuProjeto/clientes/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
+from .models import Cliente
+from .forms import ClienteForm
 
 # Create your views here.
+
+
+def person_list(request):
+    clientes = Cliente.objects.all()
+    return render(request, "clientes.html", {"clientes": clientes})
+
+
+def person_new(request):
+    form = ClienteForm(request.POST or None, request.FILES or None)
+    if form.is_valid():
+        form.save()  # Salva no banco de dados
+        return redirect("person_list")
+    return render(request, "form_cliente.html", {"form": form})
+
+
+def person_update(request, id):
+    cliente = get_object_or_404(Cliente, pk=id)
+    form = ClienteForm(request.POST or None, request.FILES or None, instance=cliente)
+    if form.is_valid():
+        form.save()
+        return redirect("person_list")
+    return render(request, "form_cliente.html", {"form": form})
+
+
+def person_delete(request, id):
+    cliente = get_object_or_404(Cliente, pk=id)
+    if request.method == "POST":
+        cliente.delete()
+        return redirect("person_list")
+    return render(request, "confirm_delete.html", {"cliente": cliente})
--- meuProjeto/meuProjeto/urls.py
+++ meuProjeto/meuProjeto/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 
 from django.conf import settings
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include  # o include chama de outro arquivo URL
 from django.conf import settings
 from django.conf.urls.static import static
 from .views import hello
@@ -27,4 +27,5 @@ urlpatterns = [
     path("pessoa/<str:nome>", pegarDados),
     path("hello/", hello),
     path("admin/", admin.site.urls),
+    path("person/", include("clientes.urls")),  # o include chama de outro arquivo URL
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
