b6d65e08f8550d623504b92f32474c41207456cc
VictorSad26 <victor.sad@hotmail.com>
2024-05-13 12:02:53 -0300

    atualizando o projeto

--- meuProjeto/meuProjeto/settings.py
+++ meuProjeto/meuProjeto/settings.py
@@ -88,12 +88,14 @@ WSGI_APPLICATION = "meuProjeto.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "gestao_clientes",
-        "USER": "postgres",
-        "PASSWORD": "123456",
-        "HOST": "127.0.0.1",
-        "PORT": "5000",
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+        # "ENGINE": "django.db.backends.postgresql",
+        # "NAME": "gestao_clientes",
+        # "USER": "postgres",
+        # "PASSWORD": "123456",
+        # "HOST": "127.0.0.1",
+        # "PORT": "5000",
     }
 }
 
